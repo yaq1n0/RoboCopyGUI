a49bf5ec5af571769f93e0ef19eb0aa6cbe0e8a8
yaq1n0 <yaqinhasan@gmail.com>
2024-01-16 22:11:26 +0000

    front end mostly done (barebones)

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,8 @@ import 'dart:developer';
 import 'package:flutter/material.dart';
 import 'package:file_picker/file_picker.dart';
 
+// enable verbose logging for debugging purposes 
+bool verbose = true;
 
 void main() {
   runApp(const RoboCopyGUIApp());
@@ -34,54 +36,155 @@ class RoboCopyGUIAppHome extends StatefulWidget {
 }
 
 class RoboCopyGUIAppHomeState extends State<RoboCopyGUIAppHome> {
-  List<String> sourceDirectories = List<String>.empty(growable: true);
+  // list of source directories to copy from
+  List<String> srcDirs = List<String>.empty(growable: true);
+  
+  // list of destination directories to copy to 
+  List<String> destDirs = List<String>.empty(growable: true);
 
-  void addToSourceDirs(String path) {
-    setState(() {
-      sourceDirectories.add(path);
-      for (String e in sourceDirectories) {
-        log(e);
-      }
-    });
-  }
+  // String to store robocopy_file.txt
+  String outputFileText = '';
 
-  void pickDirectory() async {
+  Future<void> addToSourceDirs() async {
     String? result = await FilePicker.platform.getDirectoryPath();
-
-    // exit method if nothing selected
+    
+    // nothing selected, exit method
     if (result == null) return;
 
-    addToSourceDirs(result);
+    setState(() {
+      srcDirs.add(result);
+    });
+
+    if (verbose) log('$result added to destination directories');
+  }
+
+  Future<void> addToDestDirs() async {
+    String? result = await FilePicker.platform.getDirectoryPath();
+
+    // nothing selected, exit method
+    if (result == null) return;
+
+    setState(() {
+      destDirs.add(result);
+    });
+
+    if (verbose) log('$result added to destination directories');
+  }
+
+  void readOutputFile() {
+    //TODO: implement reading robocopy_output.txt to outputfileText
+    String path = '';
+
+    String long_newline = '';
+    for (int i = 0; i < 10; i++) {
+      long_newline = '${long_newline}newline';
+    }
+    for (int i = 0; i < 10; i++) {
+      outputFileText = outputFileText + long_newline;
+    }
+
+    setState(() {});
+
+    if (verbose) log('readOutputfile method called');
+  }
+
+  void runRoboCopy() {
+    //TODO: implement actual shell interface code to run roboCopy
+    
+    if (verbose) log('runRoboCopy method called');
   }
   
   @override
   Widget build(BuildContext context) {
-    List<Widget> selectedDirs = List<Widget>.empty(growable: true);
-    selectedDirs.add(const Text('You have selected the following directories: '));
-    String selectedDirsList = '';
+    String srcDirsList = '';
+    String destDirsList = '';
 
-    for (String dirs in sourceDirectories) {
-      selectedDirsList = '$selectedDirsList$dirs\n';
+    for (String dirs in srcDirs) {
+      srcDirsList = '$srcDirsList$dirs\n';
     } 
 
-    selectedDirs.add(Text(selectedDirsList));
-    log(selectedDirsList);
+    for (String dirs in destDirs) {
+      destDirsList = '$destDirsList$dirs\n';
+    } 
+
+    Column subColumn1a = Column(
+      // selected source directories
+      children: [
+        const Text('Selected Source Directories: '), 
+        Text(srcDirsList),
+      ],
+    );
+
+    Column subColumn1b = Column(
+      // control buttons
+      children: [
+        ElevatedButton(
+          // add to source dirs
+          onPressed: addToSourceDirs, 
+          child: const Text('Add Source'),
+          ),
+        ElevatedButton(
+          // add to destination dirs
+          onPressed: addToDestDirs, 
+          child: const Text('Add Destination'),
+        ),
+        ElevatedButton(
+          // run robocopy
+          onPressed: runRoboCopy, 
+          child: const Text('Run RoboCopy'),
+          ),
+        ElevatedButton(
+          // refresh robocopy_log 
+          onPressed: readOutputFile, 
+          child: const Text('Refresh Log'),
+        ),
+      ],
+    );
+
+    Column subColumn1c = Column(
+      // selected destination directories
+      children: [
+        const Text('Selected Destination Directories: '), 
+        Text(destDirsList),
+      ],
+    );
+
+    SizedBox outputBox = SizedBox(
+      //TODO: might want to change this to relative to parent window size
+      height: 400,
+      width: 800, 
+      child: Column(
+        children: [
+          const Text('Log File (robocopy_log.txt): '),
+          Expanded(
+            child: SingleChildScrollView(
+              scrollDirection: Axis.vertical,
+              child: Text(outputFileText),
+            )
+          )
+        ]
+      ) 
+    );
+
+    Row firstRow = Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [subColumn1a, subColumn1b, subColumn1c]
+      );
 
     return Scaffold(
-      /*appBar: AppBar(
+      appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.primary,
         title: Text(widget.title),
-      ),*/
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: selectedDirs,
-        ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: pickDirectory,
-        tooltip: 'Select a Directory',
-        child: const Icon(Icons.add),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          firstRow, 
+          Row (
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [outputBox],
+          )
+        ],
       ),
     );
   }
